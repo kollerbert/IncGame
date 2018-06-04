abc941a1c42109aabf4270a252e09e514da319aa
kollerbert <39894677+kollerbert@users.noreply.github.com>
2018-06-04 17:19:43 +0200

    Update Vision.js

--- Vision.js
+++ Vision.js
@@ -1,1 +1,9 @@
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    });
+});
+</script>
