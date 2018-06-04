d79b84f57b66321b93eb1ea1a0a520759c386f74
kollerbert <39894677+kollerbert@users.noreply.github.com>
2018-06-05 01:57:05 +0200

    Update mainloop.js

--- mainloop.js
+++ mainloop.js
@@ -1,9 +1,43 @@
+var dollar = 0;
+var totalclicks=0;
+var totalclicksupgrade=0;
+var upgrade1unlock=0;
+var costU1=10;
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
-    });
-});
-</script>
+function dollarUpdate(){
+    document.getElementById("dollars").innerHTML = dollar.toPrecision(2);
+};
+
+
+function clickMe(){
+dollar=dollar + 1;
+totalclicks=totalclicks +1; 
+document.getElementById("dollars").innerHTML = dollar.toPrecision(2);
+}
+
+
+
+function upgrade1Unlock() {
+    if (dollar > costU1){
+          upgrade1unlock= upgrade1unlock + 1
+          dollar = dollar - costU1
+          costU1 = costU1*1000
+   }
+}
+
+
+function CpsGutschreiben(){
+    if (upgrade1unlock > 0){
+    dollar = dollar + (upgrade1unlock*totalclicks/1000);
+    
+}
+};
+
+
+
+window.setInterval(function(){          // funktionen 1x pro Sekunden Updaten (CPS)
+	
+    dollarUpdate();
+    CpsGutschreiben();
+	
+}, 1000);
