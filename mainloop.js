1cf193d2409642925a558d129c0803beabe9d1ea
kollerbert <39894677+kollerbert@users.noreply.github.com>
2018-06-05 02:30:48 +0200

    Update mainloop.js

--- mainloop.js
+++ mainloop.js
@@ -5,6 +5,7 @@ var upgrade1unlock=0;
 var costU1=10;
 
 function dollarUpdate(){
+
     document.getElementById("dollars").innerHTML = dollar.toPrecision(2);
 };
 
@@ -13,16 +14,19 @@ function clickMe(){
 dollar=dollar + 1;
 totalclicks=totalclicks +1; 
 document.getElementById("dollars").innerHTML = dollar.toPrecision(2);
+
 }
 
 
 
 function upgrade1Unlock() {
-    if (dollar > costU1){
-          upgrade1unlock= upgrade1unlock + 1
-          dollar = dollar - costU1
-          costU1 = costU1*1000
-   }
+if (dollar > costU1){
+    upgrade1unlock= upgrade1unlock + 1
+    dollar = dollar - costU1
+    costU1 = costU1*1000
+    document.getElementById("upgrade1unlocks").innerHTML = upgrade1unlock;
+    document.getElementById("costU1s").innerHTML = costU1.toPrecision(2);
+}
 }
 
 
@@ -34,6 +38,19 @@ function CpsGutschreiben(){
 };
 
 
+/*
+function buyCursor(){
+    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
+    if(dollar >= cursorCost){                                   //checks that the player can afford the cursor
+        cursors = cursors + 1;                                   //increases number of cursors
+    	dollar = dollar - cursorCost;                          //removes the cookies spent
+        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
+        document.getElementById('dollars').innerHTML = dollar;  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
+    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
+*/
 
 window.setInterval(function(){          // funktionen 1x pro Sekunden Updaten (CPS)
 	
