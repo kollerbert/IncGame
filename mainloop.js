5e5dd7e439e921d53d9004e5eacf62f307ba1beb
Matz Radloff <matzradloff@gmail.com>
2018-06-05 02:46:34 +0200

    Fixed indentation and stuff.

--- mainloop.js
+++ mainloop.js
@@ -1,49 +1,41 @@
 var dollar = 0;
-var totalclicks=0;
-var totalclicksupgrade=0;
-var upgrade1unlock=0;
-var costU1=10;
+var totalclicks = 0;
+var totalclicksupgrade = 0;
+var upgrade1unlock = 0;
+var costU1 = 10;
 
-function dollarUpdate(){
-
-    document.getElementById("dollars").innerHTML = dollar.toPrecision(2);
-};
-
-
-function clickMe(){
-dollar=dollar + 1;
-totalclicks=totalclicks +1; 
-document.getElementById("dollars").innerHTML = dollar.toPrecision(2);
-
+function dollarUpdate() {
+    document.getElementById('dollars').innerHTML = dollar.toFixed(2);
 }
 
-
+function clickMe() {
+    dollar = dollar + 1;
+    totalclicks = totalclicks + 1;
+    document.getElementById('dollars').innerHTML = dollar.toFixed(2);
+}
 
 function upgrade1Unlock() {
-if (dollar > costU1){
-    upgrade1unlock= upgrade1unlock + 1
-    dollar = dollar - costU1
-    costU1 = costU1*1000
-    document.getElementById("upgrade1unlocks").innerHTML = upgrade1unlock;
-    document.getElementById("costU1s").innerHTML = costU1.toPrecision(2);
-}
+    if (dollar > costU1) {
+        upgrade1unlock = upgrade1unlock + 1;
+        dollar = dollar - costU1;
+        costU1 = costU1 * 1000;
+        document.getElementById('upgrade1unlocks').innerHTML = upgrade1unlock;
+        document.getElementById('costU1s').innerHTML = costU1.toFixed(2);
+    }
 }
 
-
-function CpsGutschreiben(){
-    if (upgrade1unlock > 0){
-    dollar = dollar + (upgrade1unlock*totalclicks/1000);
-    
+function CpsGutschreiben() {
+    if (upgrade1unlock > 0) {
+        dollar = dollar + upgrade1unlock * totalclicks / 1000;
+    }
 }
-};
-
 
 /*
 function buyCursor(){
     var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
     if(dollar >= cursorCost){                                   //checks that the player can afford the cursor
         cursors = cursors + 1;                                   //increases number of cursors
-    	dollar = dollar - cursorCost;                          //removes the cookies spent
+        dollar = dollar - cursorCost;                          //removes the cookies spent
         document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
         document.getElementById('dollars').innerHTML = dollar;  //updates the number of cookies for the user
     };
@@ -52,9 +44,9 @@ function buyCursor(){
 };
 */
 
-window.setInterval(function(){          // funktionen 1x pro Sekunden Updaten (CPS)
-	
+window.setInterval(function() {
+    // funktionen 1x pro Sekunden Updaten (CPS)
+
     dollarUpdate();
     CpsGutschreiben();
-	
 }, 1000);
